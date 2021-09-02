27968006377fffb8e7324da79697ff9f2c881cc2
Charles Qian <charleshqian@gmail.com>
2021-09-02 10:08:31 -0700

    running

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require('dotenv').config() //allows you to stash artificial env variables in a file
 const express = require('express')
 const cors = require('cors')
 
 console.log(process.env.HOME)
+console.log(process.env.ANOTHER)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000 //don't need a fallback port if the dev using this has a .env
 
 const server = express()
 
