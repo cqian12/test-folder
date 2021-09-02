2f350475f8fcd3a406a8910b44c160aa423343a6
Charles Qian <charleshqian@gmail.com>
2021-09-02 09:58:03 -0700

    deployable to heroku

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const server = require('express')()
+const express = require('express')
+const cors = require('cors')
 
-server.listen(5000, () => {
-    console.log('listening on http://localhost:5000')
+console.log(process.env.HOME)
+
+const PORT = process.env.PORT || 5000
+
+const server = express()
+
+server.use(express.json())
+server.use(cors)
+
+server.get('/', (req,res) => {
+    res.send(`<h1>In progress</h1>`)
+})
+
+server.get('/api', (req,res) => {
+    res.json({message:'now a message in json'})
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
 })
 
